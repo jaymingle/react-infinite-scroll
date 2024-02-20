85a54ad45f636896efda6fd90dc4d0db6e667aa1
Jay Mingle <jkawudi@gmail.com>
2024-02-20 07:30:59 +0000

    Changes

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,6 +1,15 @@
 import React from 'react';
 
 const Posts = React.forwardRef(({post}, ref) => {
+
+    const postBody = (
+        <>
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+            <p>Post ID: {post.id}</p>
+        </>
+    )
+
     return (
         <div>
 
