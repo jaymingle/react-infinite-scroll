2cec482648fc66f40cecf34a7d480294a676c503
Jay Mingle <jkawudi@gmail.com>
2024-02-20 05:30:14 +0000

    Changes

---
+++ src/hooks/usePosts.jsx
@@ -0,0 +1,12 @@
+import React, {useState, useEffect} from 'react';
+import {getPostsPage} from "../api/axios";
+
+const usePosts = (pageNum = 1) => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default usePosts;
