8aa51d018787d40d8483ed5515fb580affb78fd6
Jay Mingle <jkawudi@gmail.com>
2024-02-20 07:18:51 +0000

    Changes

--- src/components/Example1.jsx
+++ src/components/Example1.jsx
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../App.css'
+import usePosts from "../hooks/usePosts.jsx";
 
 const Example1 = () => {
+
+    const [pageNum, setPageNum] = useState(1)
+    const {
+        isLoading,
+        isError,
+        error,
+        results,
+        hasNextPage,
+    } = usePosts(pageNum)
+
     return (
         <>
             <h1 id="top">&infin; Infinite Query &amp; Scroll<br />&infin; Ex. 1 - React only</h1>
