3109cec996920fd67040831ed8164a2db822c844
Jay Mingle <jkawudi@gmail.com>
2024-02-20 05:15:21 +0000

    Changes

--- src/components/Example1.jsx
+++ src/components/Example1.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 const Example1 = () => {
     return (
         <>
-
+            <h1 id="top">&infin; Infinite Query &amp; Scroll<br />&infin; Ex. 1 - React only</h1>
+            <p className="center">Loading More Posts...</p>
+            <p className="center"><a href="#top">Back to Top</a></p>
         </>
     );
 };
