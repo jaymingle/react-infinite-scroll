53f7fae108b64ac837ab94b642f056355d241900
Jay Mingle <jkawudi@gmail.com>
2024-02-20 06:51:15 +0000

    Changes

--- src/hooks/usePosts.jsx
+++ src/hooks/usePosts.jsx
@@ -2,11 +2,20 @@ import React, {useState, useEffect} from 'react';
 import {getPostsPage} from "../api/axios";
 
 const usePosts = (pageNum = 1) => {
-    return (
-        <div>
 
-        </div>
-    );
+    const [results, setResults] = useState([])
+    const [isLoading, setIsLoading] = useState(false)
+    const [isError, setIsError] = useState(false)
+    const [error, setError] = useState({})
+    const [hasNextPage, setHasNextPage] = useState(false)
+
+    useEffect(() => {
+
+    }, [pageNum])
+
+    return [results, isLoading, isError, error, hasNextPage]
+
+
 };
 
 export default usePosts;
