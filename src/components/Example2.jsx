f0c018770d0380bd9f8e1f213eb991790d8f4d5f
Jay Mingle <jkawudi@gmail.com>
2024-02-21 05:05:03 +0000

    Changes

--- src/components/Example2.jsx
+++ src/components/Example2.jsx
@@ -50,7 +50,7 @@ const Example2 = () => {
     return (
         <div>
             <h1 id="top">&infin; Infinite Query &amp; Scroll<br />&infin;
-                Ex. 1 - React only</h1>
+                Ex. 2 - React only</h1>
             {content}
             {isFetchingNextPage && <p className="center">Loading More Posts...</p>}
             <p className="center"><a href="#top">Back to Top</a></p>
