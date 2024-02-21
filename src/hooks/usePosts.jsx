378e943b1bbf075bf783045dccd7ff0057ebbb87
Jay Mingle <jkawudi@gmail.com>
2024-02-21 04:07:02 +0000

    Changes

--- src/hooks/usePosts.jsx
+++ src/hooks/usePosts.jsx
@@ -1,8 +1,54 @@
-import React, {useState, useEffect} from 'react';
-import {getPostsPage} from "../api/axios";
+// import React, {useState, useEffect} from 'react';
+// import {getPostsPage} from "../api/axios";
+//
+// const usePosts = (pageNum = 1) => {
+//
+//     const [results, setResults] = useState([])
+//     const [isLoading, setIsLoading] = useState(false)
+//     const [isError, setIsError] = useState(false)
+//     const [error, setError] = useState({})
+//     const [hasNextPage, setHasNextPage] = useState(false)
+//
+//     useEffect(() => {
+//         setIsLoading(true)
+//         setIsError(false)
+//         setError({})
+//
+//         const controller = new AbortController();
+//         const {signal} = controller
+//
+//         getPostsPage(pageNum, {signal})
+//             .then(data => {
+//                 setResults(prev => [...prev, ...data])
+//                 setHasNextPage(Boolean(data.length))
+//                 setIsLoading(false)
+//             })
+//             .catch( e => {
+//                 setIsLoading(false)
+//                 if (signal.aborted) return
+//                 setIsError(true)
+//                 setError({message: e.message})
+//             })
+//
+//         console.log(results)
+//
+//         return () => controller.abort()
+//
+//     }, [pageNum])
+//
+//     // console.log(results)
+//
+//     return [ isLoading, isError, error, results, hasNextPage]
+//
+//
+// };
+//
+// export default usePosts;
+
+import { useState, useEffect } from 'react'
+import { getPostsPage } from '../api/axios'
 
 const usePosts = (pageNum = 1) => {
-
     const [results, setResults] = useState([])
     const [isLoading, setIsLoading] = useState(false)
     const [isError, setIsError] = useState(false)
@@ -14,33 +60,27 @@ const usePosts = (pageNum = 1) => {
         setIsError(false)
         setError({})
 
-        const controller = new AbortController();
-        const {signal} = controller
+        const controller = new AbortController()
+        const { signal } = controller
 
-        getPostsPage(pageNum, {signal})
+        getPostsPage(pageNum, { signal })
             .then(data => {
                 setResults(prev => [...prev, ...data])
                 setHasNextPage(Boolean(data.length))
                 setIsLoading(false)
             })
-            .catch( e => {
+            .catch(e => {
                 setIsLoading(false)
                 if (signal.aborted) return
                 setIsError(true)
-                setError({message: e.message})
+                setError({ message: e.message })
             })
-
-        console.log(results)
 
         return () => controller.abort()
 
     }, [pageNum])
 
-    // console.log(results)
+    return { isLoading, isError, error, results, hasNextPage }
+}
 
-    return [ isLoading, isError, error, results, hasNextPage]
-
-
-};
-
-export default usePosts;
+export default usePosts
